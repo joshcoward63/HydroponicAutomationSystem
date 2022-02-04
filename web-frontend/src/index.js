58ca50fad8d9155dfb09e4e91674cc7f02172743
joshcoward63 <49047401+joshcoward63@users.noreply.github.com>
2022-02-04 00:29:43 -0700

    update

--- web-frontend/src/index.js
+++ web-frontend/src/index.js
@@ -4,7 +4,12 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
-
+// import { w3cwebsocket as W3CWebSocket } from "websocket";
+// const client = new W3CWebSocket('ws://127.0.0.1:8000');
+const { io } = require("socket.io-client")("https://192.168.0.91:5000");
+// const socket = io("https://127.0.0.1:5000");
+socket.on("connect_error", (err) => {  console.log(`connect_error due to ${err.message}`);});
+// socket.on("connect", () => {  console.log(socket.id); });
 ReactDOM.render(
   <React.StrictMode>
     <App />
