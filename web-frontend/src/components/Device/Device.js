3ae1616389e5c6b64d618200ec51687041daacd2
Josh <joshcoward63@yahoo.com>
2022-03-06 14:05:35 -0700

    update

--- web-frontend/src/components/Device/Device.js
+++ web-frontend/src/components/Device/Device.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Device.css"
+import client from '../../context/socket'
 
-function onButtonClick(){
-    let button1 = document.getElementById("b1");
-    if(button1.innerHTML === "On"){
-        button1.innerHTML = "Off";
-        button1.setAttribute("style", "background-color: #f44336;");
-    }
-    else{
-        button1.innerHTML = 'On';
-        button1.setAttribute("style", "background-color: #24A0ED;");
-    }
-}
-    
 
-const Device = ({}) =>{
+client.on("exhaustStatus", function(status){
+    setExhaustState(status);
+})
+
+
+const Device = ({}) =>{    
+    const [exhaustState, setExhaustState] = useState("Off");
+
+    function onButtonClick(){
+        let button1 = document.getElementById("b1");
+        
+        if(button1.innerHTML === "On"){
+            
+            setExhaustState('Off');
+            button1.setAttribute("style", "background-color: #f44336;");
+            client.emit('turnExhaustFanOff');
+        }
+        else{
+            client.emit('turnExhaustFanON');
+            setExhaustState('On');
+            button1.setAttribute("style", "background-color: #24A0ED;");
+        }
+    }
+        
     return (
         <div className='device'>
             <h4>Exhaust Fan</h4>
-            <button id = "b1" onClick={() => onButtonClick()}>On</button>
+            <button id = "b1" onClick={() => onButtonClick()}>{exhaustState}</button>
         </div>
     )
 }
